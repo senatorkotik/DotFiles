d9fd705e4409f4e6a32b6d025a6d8c99acc00de1
DesertFox <EmailNot@found.com>
2026-02-22 12:29:03 +0300

    Add patches

--- dwl/config.mk
+++ dwl/config.mk
@@ -33,4 +33,4 @@ XLIBS = xcb xcb-icccm
 # dwl itself only uses C99 features, but wlroots' headers use anonymous unions (C11).
 # To avoid warnings about them, we do not use -std=c99 and instead of using the
 # gmake default 'CC=c99', we use cc.
-CC = cc
+CC = clang

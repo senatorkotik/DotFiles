8e24db7822be20cd665b6419d3838e229ae23a8d
DesertFox <NotFound@mail.org>
2026-03-10 22:12:05 +0300

    Add screenshot to config in dwl

--- dwl/config.h
+++ dwl/config.h
@@ -140,6 +140,7 @@ static const char *menucmd[] = { "wmenu-run", NULL };
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: 2 -> at, etc. */
 	/* modifier                  key                  function          argument */
+	{ 0, XKB_KEY_Print, spawn, SHCMD("/usr/local/bin/screenshot.sh") },
 	{ MODKEY|WLR_MODIFIER_SHIFT|WLR_MODIFIER_CTRL, XKB_KEY_M,        restartdwl,     {0} },
 	{ MODKEY,                    XKB_KEY_p,           spawn,            {.v = menucmd} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,      spawn,            {.v = termcmd} },
